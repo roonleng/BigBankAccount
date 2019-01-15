4f8b0dec6808d33c22b6e323c522e59254d5669b
roonleng <42977073+roonleng@users.noreply.github.com>
2019-01-14 22:41:33 -0500

    Add files via upload

--- BigBankAccount/src/CheckingAccount.java
+++ BigBankAccount/src/CheckingAccount.java
@@ -1,7 +1,10 @@
 public  class CheckingAccount extends BankAccount
 {
+	//constants
 	/**
 	 * fees do not need to change so they are made final. 
+	 * overdraft fee is the amount of money withdrawn as penalty for when the account is overdrawn
+	 * transaction fee is incurred when the supply of free transactions has been used up
 	 * free trans is the number of free transactions allowed before the transaction fee is applied
 	 * 
 	 */
@@ -9,6 +12,15 @@ public  class CheckingAccount extends BankAccount
 	final double TRANSACTION_FEE;
 	final double FREE_TRANS;
 	int numTransactions;
+	/**
+	 * constructs a checking account with a starting balance
+	 * @param n, name: takes the name of the account
+	 * @param b, the starting balance of the account
+	 * @param odf, over draft fee, incurred when the account is overdrawn
+	 * @param tf transaction fee, incurred when the number of free transactions is exceeded
+	 * @param freeTrans, free transactions, the number of free transactions that can occur
+	 */
+
 	public CheckingAccount(String n, double b, double odf, double tf, int freeTrans)
 	{
 		super(n, b);
@@ -16,10 +28,22 @@ public  class CheckingAccount extends BankAccount
 		TRANSACTION_FEE = tf;
 		FREE_TRANS = freeTrans;
 	}
+	/**
+	 * constructs a checking account without a starting balance
+	 * @param n, name: takes the name of the account
+	 * @param odf, over draft fee, incurred when the account is overdrawn
+	 * @param tf transaction fee, incurred when the number of free transactions is exceeded
+	 * @param freeTrans, free transactions, the number of free transactions that can occur
+	 */
 	public CheckingAccount(String n, double odf, double tf, int freeTrans)
 	{
 		this(n, 0, odf, tf, freeTrans);
 	}
+	/**
+	 * deposit method takes an amount and attempts to deposit it into an account
+	 * deposit checks to see if the amount is above zero, then deposits
+	 * it also checks the free trans to see if it should incur the transaction fee
+	 */
 	public void deposit(double amt)
 	{
 		if (amt<=0)
@@ -36,38 +60,13 @@ public  class CheckingAccount extends BankAccount
 			}
 		}
 	}
-	/**public void withdraw(double amt)
-	{
-		if(amt>0)
-		{
-			if (super.getBalance()<=0)
-			{
-				throw new IllegalArgumentException();			
-			}
-			else if (super.getBalance()>=amt)
-			{
-				super.withdraw(amt);
-				numTransactions++;
-			}
-			
-			else
-				{
-					super.withdraw(OVER_DRAFT_FEE);
-					super.withdraw(amt);
-					numTransactions++;
-				}
-		}
-		else
-		{
-			throw new IllegalArgumentException();			
-
-		}
-		if(numTransactions>FREE_TRANS)
-		{
-			super.withdraw(TRANSACTION_FEE);
-		}
-	}
-	*/
+	/**
+	 * withdraw method takes an amount and attempts to with draw it from the account
+	 * it checks to see if the amount is greater than 0
+	 * checks to see if the account balance is greater than 0, wont withdraw if else
+	 * checks to see if the amount is greater than the balance, if not it withdraws and checls for transaction fee
+	 * if the amount is greater than the balance, it incurs the overdraft fee and checks trans fee
+	 */
 	public void withdraw(double amt)
 	{
 		if(amt>0)
@@ -103,6 +102,12 @@ public  class CheckingAccount extends BankAccount
 
 		}
 	}
+	/**
+	 * transfer method attempts to withdraw money the account calling the method and deposit it into another account with the same name
+	 * first checks to see if accounts have the same name; throws exception if else
+	 * then checks to see if amount is less than balance of the first account and greater than 0; throws exception if else
+	 * then it withdraws from first and deposits into second; throws exception if else
+	 */
 	public void transfer(BankAccount other, double amt)
 	{
 		if (other.getName().equals(this.getName()))
@@ -122,6 +127,10 @@ public  class CheckingAccount extends BankAccount
 			throw new IllegalArgumentException();
 		}
 	}
+	/**
+	 * unused endOfMothUpdate() method
+	 * adds interst rate to account
+	 */
 	public void endOfMonthUpdate()
 	{
 		numTransactions=0;
--- BigBankAccount/src/SavingsAccount.java
+++ BigBankAccount/src/SavingsAccount.java
@@ -1,10 +1,19 @@
 
 public class SavingsAccount extends BankAccount
 {
-	//fields
+	//fields/constants
 	double intRate;
 	final double MIN_BAL_FEE;
 	final double MIN_BAL;
+	/**
+	 * constructs a savings account with a starting balance
+	 * @param n, name: takes the name of the account
+	 * @param b, the starting balance of the account
+	 * @param intRate, the rate of interest applied to the savings account in endofthemonth and addInterest methods
+	 * @param mbf, minimum balance fee, the fee incurred when the account drops below the minimum balance
+	 * @param mb, minimum balance, the minimum ammount that must be in the account without the mbf being incurred
+	 */
+
 	public SavingsAccount(String n, double b, double r, double mb, double mbf)
 	{
 		super(n, b);
@@ -12,10 +21,23 @@ public class SavingsAccount extends BankAccount
 		MIN_BAL_FEE= mbf;
 		MIN_BAL= mb;
 	}
+	/**
+	 * constructs a checking account with a starting balance
+	 * @param n, name: takes the name of the account
+	 * @param b, the starting balance of the account
+	 * @param intRate, the rate of interest applied to the savings account in endofthemonth and addInterest methods
+	 * @param mbf, minimum balance fee, the fee incurred when the account drops below the minimum balance
+	 * @param mb, minimum balance, the minimum ammount that must be in the account without the mbf being incurred
+	 */
+
 	public SavingsAccount(String n, double r, double mb, double mbf)
 	{
 		this(n, 0, r, mb, mbf);
 	}
+	/**
+	 * deposit method takes an amount and attempts to deposit it into an account
+	 * deposit checks to see if the amount is above zero, then deposits
+	 */
 	public void deposit(double amt)
 	{
 		if (amt<0)
@@ -27,6 +49,13 @@ public class SavingsAccount extends BankAccount
 			super.deposit(amt);
 		}
 	}
+	/**
+	 * withdraw method takes an amount and attempts to with draw it from the account
+	 * it checks to see if the amount is greater than 0; throws exception if else
+	 * checks to see if the account balance is greater than amt, wont withdraw if else (wont let account go below zero)
+	 * checks to see if the amount is greater than the balance, if not it withdraws and checls for transaction fee
+	 * if the new balance is below the min bal, incurs minimum balance fee
+	 */
 	public void withdraw(double amt)
 	{
 		if (amt<=0)
@@ -49,6 +78,12 @@ public class SavingsAccount extends BankAccount
 			}
 		}
 	}
+	/**
+	 * transfer method attempts to withdraw money the account calling the method and deposit it into another account with the same name
+	 * first checks to see if accounts have the same name; throws exception if else
+	 * then checks to see if amount is less than balance of the first account and greater than 0; throws exception if else
+	 * then it withdraws from first and deposits into second; throws exception if else
+	 */
 	public void transfer(BankAccount other, double amt)
 	{
 		if (other.getName()==super.getName())
@@ -61,10 +96,19 @@ public class SavingsAccount extends BankAccount
 			throw new IllegalArgumentException();
 		}
 	}
+	/**
+	 * unused addInterest method
+	 * adds interest to account balance
+	 */
 	public void addInterest()
 	{
 		super.deposit(super.getBalance()*intRate);
 	}
+	/**
+	 * unused end of month update method
+	 * adds interest
+	 * useless
+	 */
 	public void endOfMonthUpdate()
 	{
 		this.addInterest();
